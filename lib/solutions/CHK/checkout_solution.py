97be7a02929bc284955ffe454d1a3fa0493dc270
sbx_user1051 <sbx_user1051@169.254.71.245>
2023-12-17 16:54:54 +0000

    Sun Dec 17 16:54:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,11 +4,18 @@
 # skus = unicode string
 def checkout(skus):
     #assumre for now that SKUs are given in the format of capital letters seperated by spaces "A A B B B C"
+    contents = {}
 
-    for char in skus:
+    for char in skus.split():
         if char < 'A' or char > 'Z':
             return -1
         
-        
+        contents[char] = contents.get(char, 0) + 1
+
+    total = 0
+
+    for item in contents:
+        if item == 'A':
+            total += 
 
 
