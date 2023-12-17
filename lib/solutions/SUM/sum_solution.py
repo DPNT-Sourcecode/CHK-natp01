3556d5e4da8dade0df85c2f81dd557d1bcac0774
sbx_user1051 <sbx_user1051@169.254.71.245>
2023-12-17 16:35:35 +0000

    Sun Dec 17 16:35:35 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,8 +2,9 @@
 def compute(x, y):
     return x+y
 
-def test_sum():
-    for i in range(1,100):
-        for j in range(1,100):
-            assert compute(i,j) == i+j
+# def test_sum():
+#     for i in range(1,100):
+#         for j in range(1,100):
+#             assert compute(i,j) == i+j
+
 
