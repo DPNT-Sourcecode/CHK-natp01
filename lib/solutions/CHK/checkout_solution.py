2009ca94f13acd03526d70f0f53f2bef6abe1783
sbx_user1051 <sbx_user1051@169.254.71.245>
2023-12-17 16:51:54 +0000

    Sun Dec 17 16:51:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
+    #assumre for now that SKUs are given in the format of capital letters seperated by spaces "A A B B B C"
+
+    for char in skus:
+        if char < 'A' or char > 'Z':
+            return -1
+        
+        
+
 
