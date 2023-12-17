c1449362102b8e69bca189b29c8cf0a0008a3dd0
sbx_user1051 <sbx_user1051@169.254.71.245>
2023-12-17 16:57:54 +0000

    Sun Dec 17 16:57:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,9 @@ def checkout(skus):
     #assumre for now that SKUs are given in the format of capital letters seperated by spaces "A A B B B C"
     contents = {}
 
+    if not skus:
+        return -1
+    
     for char in skus.split():
         if char < 'A' or char > 'Z':
             return -1
@@ -15,7 +18,20 @@ def checkout(skus):
     total = 0
 
     for item in contents:
+        num = contents[item]
         if item == 'A':
-            total += 
+            total += (num // 3) * 130 + (num % 3) * 50
+        if item == 'B':
+            total += (num // 2) * 45 + (num % 2) * 530
+        if item == 'C':
+            total+= 20*num
+        if item == 'D':
+            total+= 15*num
+
+    return total
+
+def test_checkout():
+    skus = []
+    assert checkout
 
 
